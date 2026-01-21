f3e3352be862646f42cb601a25f285ffaba584e0
Velimir Majstorov <majstorov@gmail.com>
2026-01-21 16:07:32 +0100

    WIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,3 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
 }
-
-subprojects {
-    configurations.configureEach {
-        resolutionStrategy {
-            force(
-                "com.google.protobuf:protobuf-java:4.28.3",
-                "com.google.protobuf:protobuf-javalite:4.28.3",
-                "com.google.protobuf:protobuf-kotlin:4.28.3",
-                "com.google.protobuf:protobuf-kotlin-lite:4.28.3",
-                "com.google.guava:guava:32.1.3-android",
-                "io.netty:netty-codec-http2:4.1.112.Final",
-                "io.netty:netty-handler:4.1.112.Final",
-                "org.jdom:jdom2:2.0.6.1",
-                "org.bitbucket.b_c:jose4j:0.9.6"
-            )
-        }
-    }
-}
