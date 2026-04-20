eccf993ed5a3ae22c8657a30ef368298e1644dc7
Velimir Majstorov <majstorov@gmail.com>
2026-04-20 11:03:49 +0200

    chore(security): force patched netty and bouncycastle in settings classpath

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,17 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
+buildscript {
+    configurations.classpath {
+        resolutionStrategy {
+            force("org.bouncycastle:bcpkix-jdk18on:1.84")
+            force("org.bouncycastle:bcprov-jdk18on:1.84")
+            force("org.bouncycastle:bcutil-jdk18on:1.84")
+            force("io.netty:netty-codec-http:4.1.132.Final")
+            force("io.netty:netty-codec-http2:4.1.132.Final")
+        }
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
